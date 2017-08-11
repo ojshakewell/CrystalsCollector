8582670f15ded013b229f4fa3c29cca117ca7df6
Brian Russell <brianrobertrussell@gmail.com>
2017-08-11 17:59:28 -0600

    final version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,96 @@
+$(function() {
 
-var target;
-var crystalValues = [];
-var playerValue;
-var wins = 0;
-var losses = 0;
+    var target;
+    var crystalValues = [];
+    var playerValue;
+    var wins = 0;
+    var losses = 0;
 
-resetNumbers();
+    resetNumbers();
 
-// $(".gem").click(function(){
+    //I tried to use a nth-child selector to get the data from the button positions, will come back to it later
+    // $(".gem").click(function(){
+    // 	updatePlayerValue($(this).data("value"));
+    //	console.log($(this).data("value"));
+    // });
 
-// 	updatePlayerValue($(this).data("value"));
-	
-// 	console.log($(this).data("value"));
-// });
+    //BUTTONS//
+    $(".red").click(function() {
 
-//BUTTONS//
-$(".red").click(function(){
+        updatePlayerValue(0);
+        isWinner();
+    });
 
-	updatePlayerValue(0);
-	isWinner();
-});
+    $(".yellow").click(function() {
 
-$(".yellow").click(function(){
+        updatePlayerValue(1);
+        isWinner();
+    });
 
-	updatePlayerValue(1);
-	isWinner();
-});
+    $(".green").click(function() {
 
-$(".green").click(function(){
+        updatePlayerValue(2);
+        isWinner();
+    });
 
-	updatePlayerValue(2);
-	isWinner();
-});
+    $(".blue").click(function() {
 
-$(".blue").click(function(){
-
-	updatePlayerValue(3);
-	isWinner();
-});
-
-//Determines if the game has been one or lost
-function isWinner(){
-
-	if (playerValue === target) {
-
-		$(".outcome").text("YOU WIN!!!");
-		wins++;
-		$(".wins").text("WINS: " + wins);
-		resetNumbers();
-
-	} else if (playerValue > target) { 
-
-		$(".outcome").text("YOU LOSE!!!");
-		losses++;
-		$(".losses").text("LOSSES: " + losses);
-		resetNumbers();
-	};
-};
-
-//Generates random number between 1 and 10
-function getRandomNumber(){
-	
-	var randomNumber = Math.floor((Math.random()*10)+1);
-	return randomNumber;
-};
-
-//creates target and crystal random numbers for a new game
-function resetNumbers(){
-
-	playerValue = 0;
-
-	target = Math.floor((Math.random()*40)+20);
-
-	$(".targetNumber").text(target);
-
-	$(".playerNumber").text(0)
-
-	for (var i = 0; i < 4; i++) {
-		crystalValues.push(getRandomNumber());
-		//$(".gem:nth-child(" + i.toString() + ")").data("value", crystalValues[i-1]);
-		console.log( crystalValues[i]);
-	}
-};
+        updatePlayerValue(3);
+        isWinner();
+    });
 
 
-//Updates the players total
-function updatePlayerValue(crystalColor){
+    //Determines if the game has been one or lost
+    function isWinner() {
 
-	playerValue += crystalValues[crystalColor];
-	$(".playerNumber").text(playerValue)
-};
+        if (playerValue === target) {
 
+            $(".outcome").text("YOU WIN!!!");
+            wins++;
+            $(".wins").text("WINS: " + wins);
+            resetNumbers();
+
+        } else if (playerValue > target) {
+
+            $(".outcome").text("YOU LOSE!!!");
+            losses++;
+            $(".losses").text("LOSSES: " + losses);
+            resetNumbers();
+        };
+    };
+
+    //Generates random number between 1 and 10
+    function getRandomNumber() {
+
+        var randomNumber = Math.floor((Math.random() * 10) + 1);
+        return randomNumber;
+    };
+
+    //creates target and crystal random numbers for a new game
+    function resetNumbers() {
+
+        playerValue = 0;
+
+        target = Math.floor((Math.random() * 40) + 20);
+
+        $(".targetNumber").text(target);
+
+        $(".playerNumber").text(0)
+
+        for (var i = 0; i < 4; i++) {
+            crystalValues.push(getRandomNumber());
+            // var nthChildString = ".gem:nth-child(" + (i+1).toString() + ")"
+            // console.log(nthChildString);
+            // $(nthChildString).data("value", crystalValues[i]);
+            // console.log($(nthChildString).data("value"))
+            console.log(crystalValues[i]);
+        }
+    };
+
+    //Updates the players total
+    function updatePlayerValue(crystalColor) {
+
+        playerValue += crystalValues[crystalColor];
+        $(".playerNumber").text(playerValue);
+    };
+}); //end $(function())
